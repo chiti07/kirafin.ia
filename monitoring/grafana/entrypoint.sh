b3e72155a8d23df8e76291908b41f4ae3bf7aaae
juan.chitiva <juan.chitiva@payu.com>
2026-06-21 22:37:44 -0500

    :sparkles: grafana and prometheus prod

--- monitoring/grafana/entrypoint.sh
+++ monitoring/grafana/entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 # Replace env vars in provisioning files before Grafana starts
 if [ -f /etc/grafana/provisioning/datasources/prometheus.yml ]; then
-    sed -i "s|\${PROMETHEUS_URL}|${PROMETHEUS_URL:-http://prometheus:9090}|g" /etc/grafana/provisioning/datasources/prometheus.yml
+    sed -e "s|\${PROMETHEUS_URL}|${PROMETHEUS_URL:-http://prometheus:9090}|g" \
+        /etc/grafana/provisioning/datasources/prometheus.yml > /tmp/prometheus.yml
+    mv /tmp/prometheus.yml /etc/grafana/provisioning/datasources/prometheus.yml
+    echo "Generated datasource:"
+    cat /etc/grafana/provisioning/datasources/prometheus.yml
 fi
 
 exec /run.sh
--- monitoring/prometheus/entrypoint.sh
+++ monitoring/prometheus/entrypoint.sh
@@ -7,9 +7,11 @@ SCHEME="${KIRA_LEDGER_SCHEME:-https}"
 # Strip scheme if the user accidentally included it
 HOST=$(echo "$HOST" | sed -e 's|^https://||' -e 's|^http://||')
 
-sed -i "s|__KIRA_LEDGER_TARGET__|$HOST|g" /etc/prometheus/prometheus.yml
-sed -i "s|__KIRA_LEDGER_SCHEME__|$SCHEME|g" /etc/prometheus/prometheus.yml
+sed -e "s|__KIRA_LEDGER_TARGET__|$HOST|g" \
+    -e "s|__KIRA_LEDGER_SCHEME__|$SCHEME|g" \
+    /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
 
-cat /etc/prometheus/prometheus.yml
+echo "Generated prometheus.yml:"
+cat /tmp/prometheus.yml
 
-exec /bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus
+exec /bin/prometheus --config.file=/tmp/prometheus.yml --storage.tsdb.path=/prometheus
